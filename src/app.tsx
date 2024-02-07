308084b0bf79e341bc91e80f24eb4c01aa79abd7
Sidnei <sidneiferreirati@gmail.com>
2024-02-07 12:50:28 -0300

    second day learning props e states

--- src/app.tsx
+++ src/app.tsx
@@ -20,9 +20,8 @@ export function App() {
       <div className='grid grid-cols-3 gap-6 auto-rows-[250px] '>
         <NewNoteCard />
 
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
+        <NoteCard note={{ date: new Date(), content: "hello World" }} />
+
 
       </div>
 
